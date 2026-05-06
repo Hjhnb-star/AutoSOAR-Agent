426eb6629357ba01d3ed4eb84e48aa7453dab8cb
Huang Jionghao <1287580642@qq.com>
2026-05-06 09:32:08 +0800

    Update ipi_defender.py

--- security_guard/ipi_defender.py
+++ security_guard/ipi_defender.py
@@ -1,1 +1,8 @@
-
+class IPIDefender:
+    @staticmethod
+    def sanitize_input(raw_text):
+        # 清洗可能包含恶意指令的外部输入
+        forbidden_tokens = ["Ignore previous instructions", "System override"]
+        for token in forbidden_tokens:
+            raw_text = raw_text.replace(token, "[REDACTED]")
+        return raw_text
